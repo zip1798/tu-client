0fba4fdf12b3ad2218351ed44f15d9719b7fb260
o.tsivun <alexandr.ts@gmail.com>
2019-07-19 23:34:33 +0300

    save auth data and restore in localstorage

--- src/main.js
+++ src/main.js
@@ -4,11 +4,21 @@ import "./plugins/base";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import {Vue2Storage} from 'vue2-storage'
 
 Vue.config.productionTip = false;
+
+Vue.use(Vue2Storage, {
+  prefix: 'tu_',
+  driver: 'local',
+  ttl: 60 * 60 * 24 * 7 * 1000
+})
 
 new Vue({
   router,
   store,
+  created() {
+    this.$store.dispatch("AUTH_RESTORE");
+  },
   render: h => h(App)
 }).$mount("#app");
--- src/store/auth.js
+++ src/store/auth.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import appConfig from "../config/app";
 import router from "../router";
+import Vue from "vue"
 
 export default {
   state: {
@@ -9,9 +10,9 @@ export default {
   },
   mutations: {
     SET_AUTH_STATUS(state, payload) {
-      console.log(payload);
       state.token = payload.token;
       state.user = payload.user;
+      Vue.$storage.set('auth', payload)
     }
   },
   actions: {
@@ -43,6 +44,12 @@ export default {
     AUTH_LOGOUT({ commit }) {
       commit("SET_AUTH_STATUS", { token: null, user: {}});
       router.push({path:"/"});
+    },
+
+    AUTH_RESTORE({ commit }) {
+      if (Vue.$storage.has('auth')) {
+        commit("SET_AUTH_STATUS", Vue.$storage.get('auth'));
+      }
     }
   },
   getters: {
